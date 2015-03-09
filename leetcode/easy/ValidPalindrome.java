802e9847a4fc26c72366b72452928cc1c734cd32
zhuyunhe <356759823@qq.com>
2015-03-10 00:56:13 +0800

    zhu

--- leetcode/easy/ValidPalindrome.java
+++ leetcode/easy/ValidPalindrome.java
@@ -1,13 +1,72 @@
 package easy;
 
+import java.util.Stack;
+
 public class ValidPalindrome {
 	public static  boolean validPalindrome(String s){
-		String[] arr = s.split(" ");
-		if(arr.length != 0){
-			char[] crr = arr[arr.length-1].toCharArray();
+		if(!("".equals(s))){
+			char[] chr = s.toUpperCase().toCharArray();
+			Stack st = new Stack<>();
+			for(char c: chr){
+				if(c>='A'&&c<='Z' || c>='0'&&c<='9'){
+						st.push(c);
+					}
+				}
+				
+				for(char c: chr){
+					if(c>='A'&&c<='Z' || c>='0'&&c<='9'){
+						
+						char tem = (char)st.pop();
+						if(tem != c){
+							return false;
+						}
+					}
+				}
+				return true;
+			    } else{
+			    	return true;
+			    }
+	}
+	
+	public static  boolean validPalindrome_1(String s){
+		if(!("".equals(s))){
+			char[] chr = s.toUpperCase().toCharArray();
+			int i = 0;
+			int j = chr.length-1;
+			
+			while(i<j){
+				if(!(isAlpha(chr[i]))){
+					++i;
+					continue;
+				}
+				if(!(isAlpha(chr[j]))){
+					--j;
+					continue;
+				}
+				if(chr[i]!=chr[j]){
+					return false;
+				}
+				i++;
+				j--;
+			}
+			
+		}
+		return true;
+	}
+	
+	private static boolean isAlpha(char c){
+		if(c>='A'&&c<='Z' || c>='0'&&c<='9'){
 			return true;
+		}
+			return false;
+	}
+	
+	public static void main(String[] args){
+		String s = "A man, a plan, a canal: Panama";
+		if(validPalindrome_1(s)){
+			System.out.println("ok");
 		} else{
-			return true;
+			System.out.println("no");
 		}
 	}
 }
--- leetcode/medium/ReverseBits.java
+++ leetcode/medium/ReverseBits.java
@@ -4,21 +4,58 @@ public class ReverseBits {
 	public static void main(String[] args){
 		int a = 43261596;
 		
-		int b = reverseBits(a);
+		int b = reverseBits_1(a);
+		
+		System.out.println(b);
 		
 		
 	}
 	
-	// you need treat n as an unsigned value
+	/**
+	 * 翻转一个整数的二进制位得到一个新的整数
+	 * @param n
+	 * @return
+	 */
     public static int reverseBits(int n) {
     	int result = 0;
     	
-    	for(int i=1; i!=0; i<<=1){
+    	for(int i=0; i<32; i++){
+    		//result先左移一位，然后取出输入n的二进制的最后一位(n & 1),用|操作把n最后一位加入result
+    		result = (result << 1) | (n & 1);
     		
+    		//输入n右移，抛弃掉最后一位
+    		n >>= 1;
     	}
 		return result;
         
     }
+    
+    /**
+     * 改进版算法，靠移位完成
+     * 中心思想是把各个位分成组，一次性交换所有两两相邻的组。然后再通过改变交换组的大小让每个位最终到达它需要去的地方。
+     * @param n
+     * @return
+     */
+    public static int reverseBits_1(int n) {
+		int result = 0;
+		
+		//相邻两位互换
+		n = ((n>>1) & 0x55555555)|((n&0x55555555)<<1);
+		
+		//每四位中的前两位和后两位互换
+		n = ((n>>2) & 0x33333333)|((n&0x33333333)<<2);
+		
+		//每八位中的前四位和后四位互换
+		n = ((n>>4) & 0x0F0F0F0F)|((n&0x0F0F0F0F)<<4);
+		
+		//每16位中的前八位和后八位互换
+		n = ((n>>8) & 0x00FF00FF)|((n&0x00FF00FF)<<8);
+		
+		//前十六位和后十六位互换，交换完成
+		n = (n>>16)|(n<<16);
+		
+    	return n;
+	}
     
     /**
      * 倒置字符串
