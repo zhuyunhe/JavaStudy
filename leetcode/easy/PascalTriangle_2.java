cb83f1ff60c8225d8e573994cfbd3e60d3ad3c15
zhuyunhe <356759823@qq.com>
2015-03-10 12:58:00 +0800

    2015-03-10-noon

--- leetcode/easy/PascalTriangle_2.java
+++ leetcode/easy/PascalTriangle_2.java
@@ -5,13 +5,26 @@ import java.util.List;
 
 public class PascalTriangle_2 {
 	public static List<Integer> getRow(int rowIndex){
-		int result = (int) Math.pow(11, rowIndex);
-		
 		List<Integer> list = new LinkedList<Integer>();
-		while(result >0){
-			
+		
+		if(rowIndex<0){
+			return list;
 		}
 		
-		return null;
+		list.add(1);
+		
+		for(int i=1; i<=rowIndex; i++){
+			for(int j=list.size()-2;j>=0;j--){
+				list.set(j+1, list.get(j)+list.get(j+1));
+			}
+			
+			list.add(1);
+		}
+		return list;
+	}
+	
+	public static void main(String[] args){
+		List<Integer> list = getRow(3);
+		System.out.println(list);
 	}
 }
