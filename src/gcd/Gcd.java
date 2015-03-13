a275f3eaab1d90a832c8d33011e1f9b953f8e230
zhuyunhe <356759823@qq.com>
2015-03-13 13:25:37 +0800

    03-13-zhu-noon

--- src/gcd/Gcd.java
+++ src/gcd/Gcd.java
@@ -1,5 +1,8 @@
 package gcd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gcd {
 	/**
 	 * 计算两个数的最大公因数的欧几里得算法
@@ -14,5 +17,40 @@ public class Gcd {
 			n = rem;
 		}
 		return m;
+	}
+	
+	public static boolean isValid(String s) {
+        boolean found = false;
+        int top = 0;
+        List<Character> a = new ArrayList<Character>();
+        for(int i=0; i<s.length(); i++){
+        	char c = s.charAt(i);
+        	
+        	if(c=='(' || c=='{' ||c=='['){
+        		a.add(c);
+        		top++;
+        	} else if(c==')' || c=='}' ||c==']'){
+
+        		if(top == 0 || (a.get(top-1)-c!=-2 && a.get(top-1)-c!=-1)){
+        			found = false;
+        			return found;
+        		} else{
+        			a.remove(--top);
+        		}
+        	}
+        }
+        if(a.size() == 0){
+        	found = true;
+        }
+		return found;
+    }
+	
+	public static void main(String[] args){
+		String s = "])";
+		if(isValid(s)){
+			System.out.println("ok");
+		} else{
+			System.out.println("no");
+		}
 	}
 }
