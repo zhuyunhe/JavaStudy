5c194456faea047bec6a62a8e68f12ca7a22721c
zhuyunhe <356759823@qq.com>
2015-05-09 22:49:00 +0800

    allrange

--- src/string/AllRange.java
+++ src/string/AllRange.java
@@ -17,14 +17,15 @@ public class AllRange {
 		c[j] = temp;
 	}
 	public static void permutation(char[] c,int begin,int len){
+		//递归退出条件，当数组中只有一个元素需要全排列时，直接输出数组即可
 		if(begin == len){
 			System.out.println(charToString(c));
 		} else{
 			//第i个数分别与它后面的数交换得到新的全排列
 			for(int i=begin; i<=len; i++){
-				swap(c,begin,i);
-				permutation(c,begin+1,len);
-				swap(c,begin,i);
+				swap(c,begin,i);	//交换数组第一个元素index和后面的某个元素x
+				permutation(c,begin+1,len);		//begin元素的后续元素递归全排列
+				swap(c,begin,i);	//将第一步交换了的index元素和x元素还原到原始位置，为下一次循环做准备，下一次循环就要交换index和另一个x了
 			}
 		}
 		
